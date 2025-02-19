dd8fe52e590e3ad248fc100075b61d1984c785d7
Aja <enyiaja@gmail.com>
2025-02-19 22:46:10 +0000

    S02V47: Styling buttons and working with padding

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice/styled_text.dart';
+//import 'package:roll_dice/styled_text.dart';
 
 const Alignment startAlignment = Alignment.topLeft;
 const Alignment endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.colors});
-
   final List<Color> colors;
+
+  void rollDice() {}
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +20,32 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello World!'),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-2.png',
+              width: 200,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: EdgeInsets.only(
+                //   top: 20,
+                // ),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              child: Text('Roll Dice'),
+            ),
+          ],
+        ),
       ),
     );
   }
